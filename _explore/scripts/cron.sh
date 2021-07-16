fede076c56b69aafd4ec70b795fc043f0fd1b7bf
Drane, Lance <dranelt@ornl.gov>
2021-07-16 14:00:02 -0400

    fix comments and run shellcheck on cron script
    Signed-off-by: Drane, Lance <dranelt@ornl.gov>

--- _explore/scripts/cron.sh
+++ _explore/scripts/cron.sh
@@ -3,12 +3,14 @@
 # This file exists in case you would like to automatically update the repository JSON files.
 
 # You can run this script with Docker via the -d or --docker flag
-# Or you can run on bare metal by excluding this flag
+# Or you can run the script directly by excluding that flag
 
 # To set up for this script, you will need to:
-# 1. Make sure 'git' and 'ssh' are installed and configured on the machine you are executing this script on.
+# 1. Make sure 'git' is installed and configured on the machine you are executing this script on.
 # 2. You will want either Docker or Python + needed dependencies to be installed on the machine.
-# 3. Clone this repository so it pulls and pushes via SSH.
+# 3. Clone this repository so it can push and pull automatically. 
+#   One example: on Gitlab, Maintainers/Owners can configure a "Project Access Token" with write_repository permissions, and then run:
+#   git clone https://oauth2:${PROJECT_ACCESS_TOKEN}@${domain}/path/to/repo
 # 4. Make sure you define the environment variable GITHUB_API_TOKEN
 # 5. Add this script to your crontab (don't move it).
 
@@ -17,7 +19,7 @@ set -euo pipefail
 
 ### branch name variables - customize these based on your own configurations ###
 CHECKOUT_BRANCH="master" # branch we initially checkout and commit to
-declare -a MERGE_BRANCHES=("dev") # array of branches we merge $CHECKOUT_BRANCH to and push to - leave as empty if this doesn't apply to you
+declare -a MERGE_BRANCHES=("dev" "main") # array of branches we merge $CHECKOUT_BRANCH to and push to - leave as empty if this doesn't apply to you
 
 ### functions ###
 
@@ -28,7 +30,7 @@ run_without_docker() {
 run_with_docker() {
     pushd ../..
     docker build -f worker.Dockerfile -t software-catalog:latest .
-    docker run --rm --name software-catalog -e GITHUB_API_TOKEN=${GITHUB_API_TOKEN} software-catalog:latest -v $PWD:/app bash -c "/app/_explore/scripts/MASTER.sh"
+    docker run --rm --name software-catalog -e GITHUB_API_TOKEN="${GITHUB_API_TOKEN}" software-catalog:latest -v "$PWD":/app bash -c "/app/_explore/scripts/MASTER.sh"
     popd
 }
 
@@ -51,7 +53,7 @@ git checkout $CHECKOUT_BRANCH
 git pull
 
 # run script based on arguments
-if [ $USE_DOCKER -eq 1]; then
+if [ $USE_DOCKER -eq 1 ] ; then
   run_with_docker
 else
   run_without_docker
@@ -63,7 +65,7 @@ git commit -m "Ran JSON collection scripts [AUTO-GENERATED]"
 git push
 
 for branch in "${MERGE_BRANCHES[@]}"; do
-    git checkout $branch
+    git checkout "$branch"
     git pull
     git merge $CHECKOUT_BRANCH
     git push
